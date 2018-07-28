6ac1226fccfe20bb868656e5910e5490b20b3b9b
Lais Vang <vang.lais05@gmail.com>
2018-07-28 16:25:09 -0500

    Created table

---
+++ client.js
@@ -0,0 +1,52 @@
+console.log('js'); //test JS
+
+$(document).ready(readyNow);
+let total = 0;
+function readyNow() {
+
+
+
+    console.log('JQ') // test JQ
+    $('#submit').click(submitButton);
+    //append to DOM
+    //  console.log(accessEmployee);
+
+}
+
+class Employee { //create class to store employee information
+    constructor(firstName, lastName, iD, jobTitle, annualSalary) {
+        this.first = firstName;
+        this.last = lastName;
+        this.id = iD;
+        this.title = jobTitle;
+        this.salary = annualSalary;
+    }
+}
+
+function submitButton() {
+    let employees = [];
+    // let newRow = $('table').append('<tr><td></td><td></td> <td></td> <td></td> <td></td></tr>')
+    let firstName = $('.firstName').val();
+    let lastName = $('.lastName').val();
+    let id = $('.id').val();
+    let jobTitle = $('.title').val();
+    let salary = parseInt($('.annualSalary').val());
+    employees.push(new Employee(firstName, lastName, id, jobTitle, salary));
+    $('.firstName').val('');
+    $('.lastName').val('');
+    $('.id').val('');
+    $('.title').val('');
+    parseInt($('.annualSalary').val('')); // clear input fields.
+    // create a new class called employee everytime information is submitted
+    // push information into an array
+    for (let emp of employees) {
+        $('table').append(`<tr><td>` + firstName + `</td><td>` + lastName + `</td> <td>` + id + `</td> <td>` + jobTitle + '</td> <td>' + salary + `</td></tr>`);
+        total = (total + emp.salary);
+        $('#total').text(`Total Monthly: $` + total);
+    }
+    function totalMonthly() {
+        if (total > 20000) {
+            $('body').css('color', 'red')
+        }
+    }
+}
