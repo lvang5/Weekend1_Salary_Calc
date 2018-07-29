43f47f4700bb2a68878662015a40791a6375d586
Lais Vang <vang.lais05@gmail.com>
2018-07-29 17:30:24 -0500

    completed base mode

--- client.js
+++ client.js
@@ -2,12 +2,13 @@ console.log('js'); //test JS
 
 $(document).ready(readyNow);
 let total = 0;
+let deleteButton = `<button class="del-btn"> Delete </button>`;
+
 function readyNow() {
-
-
 
     console.log('JQ') // test JQ
     $('#submit').click(submitButton);
+
     //append to DOM
     //  console.log(accessEmployee);
 
@@ -24,6 +25,7 @@ class Employee { //create class to store employee information
 }
 
 function submitButton() {
+
     let employees = [];
     // let newRow = $('table').append('<tr><td></td><td></td> <td></td> <td></td> <td></td></tr>')
     let firstName = $('.firstName').val();
@@ -40,13 +42,22 @@ function submitButton() {
     // create a new class called employee everytime information is submitted
     // push information into an array
     for (let emp of employees) {
-        $('table').append(`<tr><td>` + firstName + `</td><td>` + lastName + `</td> <td>` + id + `</td> <td>` + jobTitle + '</td> <td>' + salary + `</td></tr>`);
-        total = (total + emp.salary);
+        $('table').append(`<tr><td>` + firstName + `</td><td>` + lastName + `</td> <td>` + id + `</td> <td>` + jobTitle + '</td> <td>' + salary + deleteButton + `</td></tr>`);
+        // $('table').append(deleteButton)
+        total += (Math.floor((emp.salary) / 12));
         $('#total').text(`Total Monthly: $` + total);
     }
-    function totalMonthly() {
-        if (total > 20000) {
-            $('body').css('color', 'red')
-        }
+    $('.del-btn').on('click', deleteEmployee);
+    if (total > 20000) {
+        $('body').css('background-color', 'red');
     }
+
 }
+
+function deleteEmployee() {
+    console.log($(this));
+    $(this).parent().parent().remove();
+
+}
+
+//create id for each row that equals employee id
